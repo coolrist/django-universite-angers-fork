0291e0eacce42035b8c366ed981f689baff49583
coolrist <johnrist1234@gmail.com>
2024-10-23 00:56:31 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    a1_univ_angers/__pycache__/wsgi.cpython-311.pyc
    	renamed:    a1_univ_angers/__init__.py -> app/__init__.py
    	renamed:    a1_univ_angers/__pycache__/__init__.cpython-311.pyc -> app/__pycache__/__init__.cpython-311.pyc
    	renamed:    a1_univ_angers/__pycache__/settings.cpython-311.pyc -> app/__pycache__/settings.cpython-311.pyc
    	renamed:    a1_univ_angers/__pycache__/urls.cpython-311.pyc -> app/__pycache__/urls.cpython-311.pyc
    	new file:   app/__pycache__/wsgi.cpython-311.pyc
    	renamed:    a1_univ_angers/asgi.py -> app/asgi.py
    	renamed:    a1_univ_angers/settings.py -> app/settings.py
    	renamed:    a1_univ_angers/urls.py -> app/urls.py
    	renamed:    a1_univ_angers/wsgi.py -> app/wsgi.py
    	modified:   home/helpers/User.py
    	modified:   home/helpers/__pycache__/User.cpython-311.pyc
    	modified:   manage.py

--- home/helpers/User.py
+++ home/helpers/User.py
@@ -1,16 +1,41 @@
 class User:
     """
     """
+    @staticmethod
     def a_baumard(): return "a.baumard"
-    def a_caillon(): return "a.caillon"    
-    def c_durandeau(): return "c.durandeau"    
+
+    @staticmethod
+    def a_caillon(): return "a.caillon"
+
+    @staticmethod
+    def c_durandeau(): return "c.durandeau"
+
+    @staticmethod
     def c_paquereau(): return "c.paquereau"
+
+    @staticmethod
     def d_boisdron(): return "d.boisdron"
+    
+    @staticmethod
     def g_haumont(): return "g.haumont"
+    
+    @staticmethod
     def h_beaumatin(): return "h.beaumatin"
+
+    @staticmethod
     def l_hot(): return "laura.hot"
+
+    @staticmethod
     def n_calvez(): return "n.calvez"
+
+    @staticmethod
     def s_bouvier(): return "stephanie.bouvier"
+
+    @staticmethod
     def s_stephan(): return "sylvain.stephan"
+
+    @staticmethod
     def t_des(): return "th.des"
+
+    @staticmethod
     def v_wattier(): return "v.wattier"
